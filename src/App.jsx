31457428c8137b4e44c129a66cc9a28abd10d914
Sadab Azhar <sadabazhar84@gmail.com>
2024-08-15 08:35:55 +0530

    Inject Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from '../src/appwrite/auth_service.js'
+import {login, logout} from './store/authSlice.js'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if (userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> {setLoading(false)})
+  });
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap
+    content-between bg-gray-400'>
+      <div className='w-full black'>
+          <Header/>
+          <main>
+            TODO:  {/* <Outlet/> */}
+          </main>
+          <Footer/>
+      </div>
+    </div>
+  ) : null 
+
+
 }
 
 export default App
